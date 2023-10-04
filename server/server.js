839955d62835dff69d276e740acf3ee939708c2a
CSillus <71670415+csills1416@users.noreply.github.com>
2023-10-04 17:07:39 -0400

    added app.get to solve the heroku issue

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path');  // Require the path module
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/dist'));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
+
+// Add a catch-all route that sends back the main index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
